a012a2b8841fc2b0171e20e76774974f0a0f6ea9
polly-foz <polly.foz@gmail.com>
2020-06-30 19:53:03 +0800

    搭建页面结构

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Suspense,lazy} from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {Switch, Route} from 'react-router-dom';
+import Loading from "./components/Loading";
+
+const Home = lazy(()=>import('./pages/Home'))
+const History = lazy(()=>import('./pages/History'))
+const About = lazy(()=>import('./pages/About'))
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Suspense fallback={<Loading/>}>
+              <Switch>
+                <Route path="/" exact><Home/></Route>
+                <Route path="/history" exact><History/></Route>
+                <Route path="/about" exact><About/></Route>
+              </Switch>
+            </Suspense>
+
+
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
