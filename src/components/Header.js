c2109f681abbdbf6f93192d0873e246aaf17edaf
polly-foz <polly.foz@gmail.com>
2020-07-02 22:58:48 +0800

    antd的引入和基本使用

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import LogoUrl from '../logo.svg';
 import {NavLink} from 'react-router-dom';
 import styled from 'styled-components';
+import {Button} from 'antd';
 
 const Header = styled.header`
     background-color:#001529;
@@ -9,11 +10,15 @@ const Header = styled.header`
     padding:10px;
     display:flex;
     align-items: center;
-`
+`;
 
 const Logo = styled.img`
     height:30px;
-`
+`;
+
+const StyledNav = styled.nav`
+    flex:1;
+`;
 
 const StyledLink = styled(NavLink)`
     color:#fff;
@@ -21,19 +26,42 @@ const StyledLink = styled(NavLink)`
     &.active {
         border-bottom: 1px solid #fff;
     }
-`
+`;
 
-export default function() {
+const StyledButton = styled(Button)`
+    color:white;
+    margin-left:10px;
+`;
+
+const Login = styled.div`
+    
+`;
+
+export default function () {
+    const [isLogin, setIsLogin] = React.useState(false);
     return (
         <Header>
             <Logo src={LogoUrl}/>
-            <nav>
+            <StyledNav>
                 <StyledLink to='/' exact activeClassName="active">首页</StyledLink>
                 <StyledLink to='/history' exact activeClassName="active">上传历史</StyledLink>
                 <StyledLink to='/about' exact activeClassName="active">关于我</StyledLink>
-                <StyledLink to='/login' exact activeClassName="active">登录</StyledLink>
-                <StyledLink to='/register' exact activeClassName="active">注册</StyledLink>
-            </nav>
+            </StyledNav>
+            <Login>
+                {
+                    isLogin === true ?
+                        <>
+                            user A
+                            <StyledButton type="primary" onClick={()=>setIsLogin(false)}>注销</StyledButton>
+                        </>:
+                        <>
+                            <StyledButton type="primary" onClick={()=>setIsLogin(true)}>登录</StyledButton>
+                            <StyledButton type="primary">注册</StyledButton>
+                        </>
+                }
+                {/*<StyledLink to='/login' exact activeClassName="active">登录</StyledLink>*/}
+                {/*<StyledLink to='/register' exact activeClassName="active">注册</StyledLink>*/}
+            </Login>
 
         </Header>
     );
