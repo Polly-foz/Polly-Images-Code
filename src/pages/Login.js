fe3213f9cb202230a2f319fb01cb66da80d11f5f
polly-foz <polly.foz@gmail.com>
2020-07-03 22:32:42 +0800

    登录/注册页面 UI与表单验证

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,99 @@
-import React, {createRef} from "react";
-import {observer} from "mobx-react";
-import {useStores} from "../stores";
+import React from "react";
+import {Form, Input, Button} from 'antd';
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+    max-width: 600px;
+    margin: 10px auto;
+    padding: 10px;
+    box-shadow: 1px 1px 1px rgba(0,0,0,0.2);
+`;
 
-const Component = observer(()=>{
-    const {AuthStore} = useStores()
-    const inputRef = createRef()
+const Title = styled.h1`
+    text-align: center;
+`;
 
-    const bindChange = e=>{
-        AuthStore.setUsername(inputRef.current.value)
-    }
+const layout = {
+    labelCol: {
+        span: 6,
+    },
+    wrapperCol: {
+        span: 18,
+    },
+};
+const tailLayout = {
+    wrapperCol: {
+        offset: 6,
+        span: 18,
+    },
+};
+
+const Component = () => {
+    const onFinish = values => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = errorInfo => {
+        console.log('Failed:', errorInfo);
+    };
 
     return (
-        <>
-            <h1>Login: {AuthStore.values.username}</h1>
-            <input onChange={bindChange} ref={inputRef}/>
-        </>
-    )
-})
-export default Component
+        <Wrapper>
+            <Title>登录</Title>
+            <Form
+                {...layout}
+                name="basic"
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+            >
+                <Form.Item
+                    label="用户名"
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: '输入用户名',
+                        },
+                        {
+                            min:4,
+                            max:10,
+                            message:'长度为4～10个字符'
+                        },
+                        ()=>({
+                            validator(rule,value){
+                                if(/\W/.test(value)){
+                                    return Promise.reject('只能包含数字字母和下划线')
+                                }
+                                return Promise.resolve();
+                            }
+                        })
+                    ]}
+                >
+                    <Input/>
+                </Form.Item>
+
+                <Form.Item
+                    label="密码"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: '输入密码',
+                        },
+                    ]}
+                >
+                    <Input.Password/>
+                </Form.Item>
+
+                <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit">
+                        登录
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Wrapper>
+    );
+};
+
+export default Component;
+
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,16 +1,123 @@
 import React from "react";
-import {useStores} from "../stores";
-import {observer} from "mobx-react";
+import {Form, Input, Button} from 'antd';
+import styled from "styled-components";
 
-const Component = observer(()=>{
-    const {AuthStore} = useStores()
+const Wrapper = styled.div`
+    max-width: 600px;
+    margin: 10px auto;
+    padding: 10px;
+    box-shadow: 1px 1px 1px rgba(0,0,0,0.2);
+`;
+
+const Title = styled.h1`
+    text-align: center;
+`;
+
+const layout = {
+    labelCol: {
+        span: 6,
+    },
+    wrapperCol: {
+        span: 18,
+    },
+};
+const tailLayout = {
+    wrapperCol: {
+        offset: 6,
+        span: 18,
+    },
+};
+
+const Component = () => {
+    const onFinish = values => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = errorInfo => {
+        console.log('Failed:', errorInfo);
+    };
 
     return (
-        <>
-            <h1>Register: {AuthStore.values.username}</h1>
-        </>
-    )
-})
+        <Wrapper>
+            <Title>注册</Title>
+            <Form
+                {...layout}
+                name="basic"
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+            >
+                <Form.Item
+                    label="用户名"
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: '输入用户名',
+                        },
+                        {
+                            min:4,
+                            max:10,
+                            message:'长度为4～10个字符'
+                        },
+                        ()=>({
+                            validator(rule,value){
+                                if(/\W/.test(value)){
+                                    return Promise.reject('只能包含数字字母和下划线')
+                                }
+                                return Promise.resolve();
+                            }
+                        })
+                    ]}
+                >
+                    <Input/>
+                </Form.Item>
 
-export default Component
+                <Form.Item
+                    label="密码"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: '输入密码',
+                        },
+                    ]}
+                >
+                    <Input.Password/>
+                </Form.Item>
+
+                <Form.Item
+                    label="确认密码"
+                    name="confirmPassword"
+                    dependencies={['password']}
+                    hasFeedback
+                    rules={[
+                        {
+                            required: true,
+                            message: '再次确认密码',
+                        },
+                        ({ getFieldValue }) => ({
+                            validator(rule, value) {
+                                if (!value || getFieldValue('password') === value) {
+                                    return Promise.resolve();
+                                }
+
+                                return Promise.reject('两次输入的密码不一致!');
+                            },
+                        }),
+                    ]}
+                >
+                    <Input.Password/>
+                </Form.Item>
+
+                <Form.Item {...tailLayout}>
+                    <Button type="primary" htmlType="submit">
+                        注册
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Wrapper>
+    );
+};
+
+export default Component;
 
