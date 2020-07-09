61df77d3405fd0c7aa0cd0ba0d27c02f794297e2
polly-foz <polly.foz@gmail.com>
2020-07-09 12:37:28 +0800

    antd实现点击和拖拽上传

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,34 +1,45 @@
 import React, {useRef} from 'react';
 import {useStores} from '../stores/index';
 import {observer} from "mobx-react";
+import { Upload } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+
+const { Dragger } = Upload;
 
 const Component = observer(() => {
-    const ref = useRef();
     const {ImageStore} = useStores();
-    const bindChange = () => {
-        console.log(ref.current);
-        window.file = ref.current;
-        ImageStore.setFile(ref.current.files[0]);
-        ImageStore.setFilename(ref.current.files[0].name);
-        ImageStore.upload().then(serverFile => {
-            console.log("上传成功");
-            console.log(serverFile);
-        }).catch(error => {
-            console.log("上传失败");
-        });
-    };
+
+    const props = {
+        beforeUpload: file => {
+            ImageStore.setFile(file);
+            ImageStore.setFilename(file.name);
+            ImageStore.upload().then(serverFile => {
+                console.log("上传成功");
+                console.log(serverFile);
+            }).catch(error => {
+                console.log("上传失败");
+            });
+            // console.log(file)
+            return false;
+        },
+        showUploadList:false
+    }
     return (
         <div>
-            <h1>Uploader</h1>
-            <input type="file" ref={ref} onChange={bindChange}/>
-            {ImageStore.isUploading ?
-                <div>正在上传。。。</div> :
-                (ref.current ?
-                        <div>上传完成</div> :
-                        <></>
-
-                )
-            }
+            <h1>上传图片</h1>
+            <Dragger {...props}>
+                <p className="ant-upload-drag-icon">
+                    <InboxOutlined />
+                </p>
+                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                <p className="ant-upload-hint">
+                    Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+                    band files
+                </p>
+            </Dragger>
+            {ImageStore.serverFile?<div>
+                {ImageStore.serverFile.attributes.url.attributes.url}
+            </div>:null}
         </div>
     );
 });
