933ab649b2d695ba87d083f3bf9bbbb3bea6fe91
polly-foz <polly.foz@gmail.com>
2020-07-19 10:17:20 +0800

    实现名字筛选

--- src/components/List.js
+++ src/components/List.js
@@ -2,14 +2,14 @@ import React, {useEffect} from "react";
 import {observer} from "mobx-react";
 import {useStores} from '../stores/index';
 // import {List,Spin} from "antd";
-import {Space, Button, Table, DatePicker, Spin} from "antd";
-import InfiniteScroll from 'react-infinite-scroller';
+import {Input, Table, DatePicker, Spin, Row, Col} from "antd";
 import styled from "styled-components";
 import dayjs from 'dayjs';
 
 const Component = observer(() => {
     const {HistoryStore} = useStores();
     const {RangePicker} = DatePicker;
+    const {Search} = Input;
 
     useEffect(() => {
         console.log('进入组件');
@@ -91,20 +91,34 @@ const Component = observer(() => {
         console.log('onOk', value);
     };
 
+    let dateFilter = ()=>true;
+    let nameFilter = ()=>true;
 
     function onChange(value, dateString) {
         console.log('Selected Time: ', value);
         console.log('Formatted Selected Time: ', dateString);
         if (dateString[0] === '' || dateString[1] === '') {
-            HistoryStore.setFilter(() => true);
+            dateFilter = () => true;
+        } else if (dateString[0] === dateString[1]) {
+            dateFilter = (item) => dayjs(item.createdAt).isSame(dateString[0], 'day');
+            // return dayjs(item.createdAt).isSame(dateString[0],'day')
+        } else {
+            dateFilter = item => dayjs(item.createdAt).isBefore(dayjs(dateString[1]).add(1, 'day')) && dayjs(item.createdAt).isAfter(dayjs(dateString[0]).subtract(1, 'day'));
+            // return dayjs(item.createdAt).isBefore(dayjs(dateString[1]).add(1, 'day')) && dayjs(item.createdAt).isAfter(dayjs(dateString[0]).subtract(1, 'day'));
         }
-        HistoryStore.setFilter(item => {
-            if(dateString[0] === dateString[1]){
-                return dayjs(item.createdAt).isSame(dateString[0],'day')
-            }
-            return dayjs(item.createdAt).isBefore(dayjs(dateString[1]).add(1, 'day')) && dayjs(item.createdAt).isAfter(dayjs(dateString[0]).subtract(1, 'day'));
-        });
+        HistoryStore.setFilter(item=>{return dateFilter(item)&&nameFilter(item)})
     }
+
+
+    function onSearch(value) {
+        console.log('search', value);
+        if(value===''){
+            dateFilter = ()=>true
+        }else{
+            dateFilter = item=>item.attributes.filename.indexOf(value)>=0
+        }
+        HistoryStore.setFilter(item=>{return dateFilter(item)&&nameFilter(item)})
+    };
 
     const Img = styled.img`
         height:200px;
@@ -117,11 +131,26 @@ const Component = observer(() => {
         margin: 30px 0;
     `;
 
+    const StyledRow = styled(Row)`
+        margin: 10px 0;
+    `;
 
     return (
         <>
-            选择起始时间：<RangePicker onOK={onOk} onChange={onChange} disabled={HistoryStore.isLoading}/>
-
+            <StyledRow>
+                <Col span={3}>起始时间：</Col>
+                <Col>
+                    <RangePicker onOK={onOk} onChange={onChange} disabled={HistoryStore.isLoading}/>
+                </Col>
+            </StyledRow>
+            <StyledRow>
+                <Col span={3}>
+                    名字：
+                </Col>
+                <Col>
+                    <Search placeholder='名字' style={{width: 200}} onSearch={onSearch}/>
+                </Col>
+            </StyledRow>
             <SpinWrapper>
                 <Spin spinning={HistoryStore.isLoading} tip='正在加载中...'></Spin>
             </SpinWrapper>
